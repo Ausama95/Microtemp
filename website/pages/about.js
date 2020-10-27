9f1ed670b059718db10969e77fc8d9ccbd41f6e9
Osama Ahmed PC Main <Ausama95@yahoo.com>
2020-10-28 00:50:59 +0100

    Updated the login page and the about page

--- website/pages/about.js
+++ website/pages/about.js
@@ -1,25 +1,37 @@
 import styles from '../styles/Home.module.css'
 
 function About() {
-    return(
+    return (
         <div className={styles.container}>
 
             <main className={styles.main}>
-                <h1 className={styles.title}>About page</h1>
+
+                {/* <h1 className={styles.title}>About</h1> */}
+                
+                <div className={styles.aboutsection}>
+                    <h1>About The Page</h1>
+                    <p>This website helps you keep track of the room's temperature & light level and gives you a live feed for every second.</p>
+                    <p>With the help of a Micro:bit chip.</p>
+                </div>
+
+                <p className={styles.description}>
+                    This website was developed by: {' '}
+                    <code className={styles.code}>Osama Khalid Walid Ahmed</code>
+                </p>
             </main>
-            
+
             <footer className={styles.footer}>
                 <a
-                /* href="#" */
-                target="_blank"
-                rel="noopener noreferrer"
+                    /* href="#" */
+                    target="_blank"
+                    rel="noopener noreferrer"
                 >
-                Made by: {' '}
-                <img src="/Ausama95.png" alt="Ausama95 Logo" className={styles.logo} />
+                    Made by: {' '}
+                    <img src="/Ausama95.png" alt="Ausama95 Logo" className={styles.logo} />
                 </a>
             </footer>
         </div>
-      
+
     )
 }
 
--- website/pages/users/edit_info.js
+++ website/pages/users/edit_info.js
@@ -2,12 +2,38 @@ import styles from '../../styles/Home.module.css'
 import { useState } from 'react';
 import fire from '../../components/firebase';
 import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import { firestore } from "../../components/firebase";
 
 const CreatePost = () => {
-    const [room_name, setRoom_name] = useState('');
-    const [chip_name, setChip_name] = useState('');
+    const [room_name2, setRoom_name] = useState('');
+    const [chip_name2, setChip_name] = useState('');
     const [notification, setNotification] = useState('');
     const router = useRouter();
+    const [data2, setdata2] = useState('');
+
+    useEffect(() => {
+        return firestore
+            .collection("Microbit")
+            .doc("Room_Info")
+            .onSnapshot(
+                (snapshot) => {
+                    setdata2(snapshot.data());
+                }
+            );
+    }, []);
+
+    function required() {
+        var empt = document.forms["form1"]["text1"].value;
+        if (empt == "") {
+            alert("Please input a Value");
+            return false;
+        }
+        else {
+            alert('Code has accepted : you can try another');
+            return true;
+        }
+    }
 
     const handleSubmit = (event) => {
         event.preventDefault();
@@ -16,8 +42,8 @@ const CreatePost = () => {
             .collection("Microbit")
             .doc("Room_Info")
             .set({
-                room_name: room_name,
-                chip_name: chip_name,
+                room_name: room_name2,
+                chip_name: chip_name2,
             });
 
         setRoom_name('');
@@ -37,14 +63,14 @@ const CreatePost = () => {
                 <div>
                     <h1>Edit info</h1>
                     {notification}
-                    <form onSubmit={handleSubmit}>
-                        Room name<input className={styles.input} type="text" value={room_name}
+                    <form name="form1"  onSubmit={handleSubmit}>
+                        Room name<input className={styles.input} type="text" name="fname" value={room_name2} placeholder={data2.room_name} onsubmit="required()"
                             onChange={({ target }) => setRoom_name(target.value)} />
                         <br />
-                        Chip name<input className={styles.input} type="text" value={chip_name}
+                        Chip name<input className={styles.input} type="text" name="fname" value={chip_name2} placeholder={data2.chip_name} onsubmit="required()"
                             onChange={({ target }) => setChip_name(target.value)} />
                         <br />
-                        <button className={styles.button} type="submit">Save</button>
+                        <button className={styles.button} type="submit" value="Submit">Save</button>
                     </form>
                 </div>
             </main>
